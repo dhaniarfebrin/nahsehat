747216699af65039ee08ab79d563c5820436be55
dhaniarfebrinm <dhaniarfebrinwahyudi@gmail.com>
2022-11-24 14:13:21 +0700

    initial commit

---
+++ NahSehat/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NahSehat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = new Home();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Home();
+        }
+
+        private void News_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new News();
+        }
+
+        private void Dieseas_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Dieseas();   
+        }
+
+        private void Covid_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Covid();
+        }
+    }
+}
